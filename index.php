1c842be1a3435c2e9947cf61f88f3dbe0e2763f8
amelie_brizard <amelie.brizard@etu.univ-orleans.fr>
2024-01-15 15:26:00 +0100

    Commencement de l'interface

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <html>
   <head>
+    <meta charset="utf-8">
     <title>Premier programme PHP !</title>
+    <link rel="stylesheet" href="./data/style.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Lato'>
   </head>
   <body>
-    <!-- redirige vers quiz.php -->
-    <a href="quiz.php">quiz</a>
-    <a href = "Question.php"> Question </a>
+    <header>
+      <h1>Quiz</h1>
+    </header>
+    <main>
+      <?php
+      require "quiz.php";
+      ?>
+    </main>
+    <footer>
+      <p>test</p>
+    </footer>
   </body>
 </html>
